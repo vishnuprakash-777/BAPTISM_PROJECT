3b3217938ac1d72aebb22a0e083cd71f2f66036d
Vishnu Prakash J <82284517+vishnuprakash-777@users.noreply.github.com>
2025-07-12 21:24:44 +0530

    Update settings.py

--- baptism_project/settings.py
+++ baptism_project/settings.py
@@ -148,7 +148,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'baptismreply@gmail.com'
-EMAIL_HOST_PASSWORD = 'hzzynzzxqtdbgitz'
+EMAIL_HOST_PASSWORD = 'hzzynzzxqtdbgit'
 
 '''import certifi
 import ssl
